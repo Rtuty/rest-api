568b813a5a256549753bef6b84f4551b06e0655c
Kirill Kudryavcev <rtutiui@mail.ru>
2022-08-09 16:07:47 +0300

    nothing wrong. trust me...

--- cmd/main/test.go
+++ cmd/main/test.go
@@ -25,11 +25,31 @@ var kirill human = human{
 	},
 }
 
+func structPlay() {
+	//игрульки со структурами
+	*ageKirillPointer = 19
+	*contactPhonePointer = 12398731987
+	if kirill.age == 19 {
+		fmt.Println(kirill.age, kirill.name, kirill.contact.phoneNumber)
+	} else {
+		panic("слава вове putinu")
+	}
+}
+
+var numbers = [...]string{"Kirill", "Kudryavcev"}
+var numbers2 [5]string = [5]string{"1", "2", "3", "4"}
+var numbers3 = [5]string{"4", "3", "2", "1"}
+
+func massivePerebor(massive []string) {
+	defer fmt.Println(numbers2, numbers3)
+	for index, value := range massive {
+		fmt.Println(value, index)
+	}
+}
+
 var ageKirillPointer *int = &kirill.age
 var contactPhonePointer *int = &kirill.contact.phoneNumber
 
 func main() {
-	*ageKirillPointer = 19
-	*contactPhonePointer = 12398731987
-	fmt.Println(kirill.age, kirill.name, kirill.contact.phoneNumber)
+	massivePerebor(numbers[:])
 }
